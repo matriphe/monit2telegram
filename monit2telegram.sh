3ac3c1092373afb000b9265316ad67bd48ba1a7b
Muhammad Zamroni <halo@matriphe.com>
2016-08-18 16:20:28 +0700

    Update script

--- monit2telegram.sh
+++ monit2telegram.sh
@@ -2,7 +2,7 @@
 #
 # Monit EXEC handler that sends monit notifications via Telegram
 #
-# Depends on having `sendtelegram.sh` installed and a config file in `/root/.telegramrc`
+# Depends on having `/usr/local/bin/sendtelegram` installed and a config file in `/root/.telegramrc`
 #
-message="Monit: $MONIT_SERVICE - $MONIT_EVENT at $MONIT_DATE on host '$MONIT_HOST': $MONIT_DESCRIPTION."
-/usr/local/bin/sendtelegram -c /root/.twiliorc -m $message
+message="Monit $MONIT_SERVICE - $MONIT_EVENT at $MONIT_DATE on $MONIT_HOST: $MONIT_ACTION $MONIT_DESCRIPTION."
+/usr/local/bin/sendtelegram -c /root/.telegramrc -m $message
